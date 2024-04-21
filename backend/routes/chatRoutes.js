1a1512f52719263b1ad91495a81deddd1e1824cc
Koby Smith-McCallister <kemccallister2002@gmail.com>
2024-04-20 22:43:16 -0500

    Finished all of the chat routes

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const { protect } = require("../middleware/authMiddleware");
-const { accessChat } = require("../controllers/chatControllers");
-const { fetchChats } = require("../controllers/chatControllers");
+const {
+  accessChat,
+  fetchChats,
+  createGroupChat,
+  renameGroup,
+  addToGroup,
+  removeFromGroup,
+} = require("../controllers/chatControllers");
 
 const router = express.Router();
 
 router.route("/").post(protect, accessChat);
 router.route("/").get(protect, fetchChats);
-// router.route("/group").post(protect, createGroup);
-// router.route("/rename-group").put(protect, renameGroup);
-// router.route("/remove-group").put(protect, removeGroup);
-// router.route("/add-to-group").pu(protect, addToGroup);
+router.route("/group").post(protect, createGroupChat);
+router.route("/rename-group").put(protect, renameGroup);
+router.route("/add-to-group").put(protect, addToGroup);
+router.route("/remove-from-group").put(protect, removeFromGroup);
 
 module.exports = router;
